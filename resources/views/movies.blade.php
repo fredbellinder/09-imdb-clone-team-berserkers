e1049e8f45503089e947951c382b4d2fdcde8755
daniel-salin <daniel.salin@chasacademy.se>
2019-03-21 08:49:43 +0100

    :lipstick: Add basic (inline) styling, poster icon and year of release to search results

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.master')
 
 @section('content')
-    <h2>TMDB Movies Search Query Results</h2>
-    <ul>
-        @foreach ($results as $match)
-            <li>{{ $match->title }}
-        @endforeach
-
-    </ul>
+  <h2>TMDB Movies Search Query Results</h2>
+  <ul style="list-style-type:none">
+    @foreach ($results as $match)
+    	<li>
+				<div style="margin:10px">
+					<img src="http://image.tmdb.org/t/p/w45//{{$match->poster_path}}">
+      	  <a href="#" style="text-decoration:none;color:black"><b>{{ $match->title }}</b> ({{ $match->release_date }})</a>
+      	<div> 
+      </li>
+    @endforeach
+  </ul>
 @endsection
