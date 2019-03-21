b32d3d89408adce488ad9eda11f9d8d37989731c
Fred Bellinder <fred.bellinder@chasacademy.se>
2019-03-21 15:16:56 +0100

    Safetycommit before merge

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,13 +1,11 @@
-@extends('layouts.master')
-
+@extends('layouts.master') 
 @section('content')
-    <div class="card m-4" style="width: 18rem;">
-            <img class="card-img-top" src="http://image.tmdb.org/t/p/w300//{{$movie->poster_path}}" alt="Card image cap">
-            <div class="card-body">
-            <h5 class="card-title">{{ $movie->original_title }} ({{ $movie->release_date }})</h5>
-              <p class="card-text">{{ $movie->overview }}</p>
-            </div>
-          </div>
-        <div> 
-
+<div class="card m-4" style="width: 18rem;">
+    <img class="card-img-top" src="http://image.tmdb.org/t/p/w300//{{$movie->poster_path}}" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">{{ $movie->original_title }} ({{ $movie->release_date }})</h5>
+        <p class="card-text">{{ $movie->overview }}</p>
+    </div>
+</div>
+<div>
 @endsection
