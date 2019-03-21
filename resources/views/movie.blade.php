080be67b4c3b11da4f8509ee6472de81e5518377
daniel-salin <daniel.salin@chasacademy.se>
2019-03-21 15:22:34 +0100

    Add form inputs

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -6,6 +6,15 @@
             <div class="card-body">
             <h5 class="card-title">{{ $movie->original_title }} ({{ $movie->release_date }})</h5>
               <p class="card-text">{{ $movie->overview }}</p>
+              <form class="form-inline my-2 my-lg-0" method="POST" action="/watchlist">
+                <input name="title" value="{{ $movie->original_title }}" hidden />
+                <input name="movie_id" value="{{ $movie->id }}" hidden /> 
+                <input name="poster_url" value="{{ $movie->poster_path }}" hidden />
+                <input name="user_id" value="1" hidden />
+                <input name="list_id" value="1" hidden />
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Add to list</button>
+              </form>
+              <a href="/watchlist" style="text-decoration:none;color:black">Add Movie to Watchlist</a>
             </div>
           </div>
         <div> 
