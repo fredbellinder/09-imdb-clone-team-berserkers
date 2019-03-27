ca9cc607b53680cd5458539aaff342ef5e7f61c6
Christian Blom <christian.blom@chasacademy.se>
2019-03-27 15:32:18 +0100

    Old welcome blade is now toplists.toplists which accepts title and movie list params to render a top list of movies. Welcome view is now a placeholder for the index/starting page. Closes #83.

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('welcome');
+    }
+
+    public function showMostPopularOfTheYear()
+    {
+        $client = new \GuzzleHttp\Client();
+        
+        $apikey = env('TMDB_API_KEY', '');
+        $current_year = date("Y");
+        
+        $popular_fetch = $client->get("https://api.themoviedb.org/3/discover/movie?api_key=$apikey&sort_by=popularity.desc&page=1&primary_release_year=$current_year");
+        
+        $response = json_decode($popular_fetch->getBody());
+
+        $movies = array_slice($response->results, 0, 5);
+
+        return view('toplists.toplists', [
+            'title' => "Hottest of $current_year",
+            'list' => $movies
+        ]);
+    }
+
+    public function showTopHorrorMovies()
+    {
+        $client = new \GuzzleHttp\Client();
+        $apikey = env('TMDB_API_KEY', '');
+
+        $horror_fetch = $client->get("https://api.themoviedb.org/3/discover/movie?api_key=$apikey&sort_by=vote_average.desc&with_genres=27&vote_count.gte=50");
+
+        $response = json_decode($horror_fetch->getBody());
+        $movies = array_slice($response->results, 0, 5);
+
+        return view('toplists.toplists', [
+            'title' => 'Top rated horror movies',
+            'list' => $movies
+        ]);
+    }
+}
---
+++ resources/views/toplists/toplists.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('content')
+
+<h1 class="text-center">{{ $title }}</h1>
+  @foreach ($list as $entry)
+      <div class="card w-100 text-center">
+        <div class="welcome-container" style="background-image:url('https://image.tmdb.org/t/p/w1280/{{$entry->backdrop_path}}');">
+          <div class="welcome-container-info">
+            <h2 class="align-bottom"><a class="text-berserker-primary" href="/movies/{{$entry->id}}">{{$entry->title}}</a></h2>
+            <h3 class="align-bottom">{{$entry->vote_count}} votes</h3>
+            <h3 class="align-bottom">{{$entry->vote_average}} average grade</h3>
+            <h3 class="align-bottom">{{$entry->popularity}} popularity</h3>
+          </div>
+        </div>
+      </div>
+  @endforeach
+
+@endsection
