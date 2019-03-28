7f94dc5decf5ce61cab0d42a74506bd93903c436
daniel-salin <daniel.salin@chasacademy.se>
2019-03-28 14:14:38 +0100

    :lipstick: Add logo, move left side nav items to right hand login nav

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,19 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">IMDB CLONE</a>
+  <a class="nav-link" href="/">
+      <img class="logo" width="125" src="{{ asset('assets/logo.svg') }}" />
+    </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#featureContent" aria-controls="featureContent"
     aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
   <div class="collapse navbar-collapse" id="featureContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/watchlists">Watchlists</a>
-      </li>
-    </ul>
     <form class="form-inline my-2 my-lg-0" method="GET" action="/movies">
       <input name="query" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" required>
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
@@ -24,13 +18,7 @@
       aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="loginContent">
-      <!-- Left Side Of Navbar -->
-      <ul class="navbar-nav mr-auto">
-
-      </ul>
-
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav ml-auto">
         <!-- Authentication Links -->
@@ -46,18 +34,13 @@
         <li class="nav-item dropdown">
           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
+            {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-            
-            <a class="dropdown-item" href="/users">
-                            {{ __('Dashboard') }}
-                        </a>
+              document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+            <a class="dropdown-item" href="/watchlists">{{ __('Watchlists') }}</a>
+            <a class="dropdown-item" href="/users">{{ __('Dashboard') }}</a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
               @csrf
             </form>
