fe4312eec615e50595208596e6daf50166100410
daniel-salin <daniel.salin@chasacademy.se>
2019-04-04 12:45:46 +0200

    :lipstick: Darken text link color and make movies posters linked to their respective movie view

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -6,10 +6,12 @@
     @foreach ($results as $match)
     	<li>
 				<div class="m-1 match-container">
+            <a href="/movies/{{ $match->id}}">
             @if($match->poster_path !== null)
             <img src="http://image.tmdb.org/t/p/w45//{{$match->poster_path}}">
             @else
             <img src="https://via.placeholder.com/45x68.png?text=X" alt="{{ $match->title }}"/>
+          </a>
             @endif
       	  <a href="/movies/{{ $match->id}}"><b>{{ $match->title }}</b> ({{ $match->release_date }})</a>
       	<div> 
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -16,7 +16,7 @@
     <h2>My Reviews</h2>
     <ul> @if (count($reviews) > 0) @foreach ($reviews as $entry)
       <li>
-        <h3><a href="/reviews/{{$entry->id}}?movie_id={{ $entry->movie_tmdb_id}}">{{ $entry->movie_title }}</a></h3>
+        <h3><a class="text-dark" href="/reviews/{{$entry->id}}?movie_id={{ $entry->movie_tmdb_id}}">{{ $entry->movie_title }}</a></h3>
         @if($entry->rating === null)
         <img src="{{ asset('assets/null.svg') }}" /> @else
         <img src="{{ asset('assets/'.($entry->rating*10).'.svg') }}" /> @endif
