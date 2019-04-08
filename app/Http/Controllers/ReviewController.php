1205c9aac3ce63a47cc344c181beb1f4b5669b8e
Fred B <fred.goteborg@gmail.com>
2019-04-08 15:43:33 +0200

    :construction: Add deletebutton and functionality to reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -123,8 +123,19 @@ class ReviewController extends Controller
      * @param  \App\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Review $review, Request $request)
     {
-        //
+        $user_id = $request->user()->id;
+        $review_id = $review->id;
+
+        $toDelete = Review::where(
+            'id',
+            $review_id
+        )->where(
+            'user_id',
+            $user_id
+        )->delete();
+
+        return redirect()->back()->withErrors(['Something went wrong']);
     }
 }
